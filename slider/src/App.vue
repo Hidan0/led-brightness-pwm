2a131f7e20ba520a5124e4b1ef83466b801aae53
Hidan0 <48904232+Hidan0@users.noreply.github.com>
2023-09-07 18:42:45 +0200

    [ADD] web app slider

---
+++ slider/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Slider from "@/components/Slider.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="title">Led brightness</h1>
+    <Slider />
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/main";
+
+.title {
+  text-align: center;
+  padding: 1rem 0;
+}
+</style>
---
+++ slider/src/components/Slider.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import axios from "axios";
+
+const brightness = ref(0);
+const sliderValue = computed(() => `${brightness.value}%`);
+
+const BOARD_IP = "192.168.1.17";
+
+const handleChange = async () => {
+  try {
+    await axios.put(`http://${BOARD_IP}/brightness?${brightness.value}`);
+  } catch (error: any) {
+    console.log(error.message);
+  }
+};
+</script>
+
+<template>
+  <div class="s-wrapper">
+    <input
+      class="slider"
+      type="range"
+      min="0"
+      max="100"
+      id="led-slider"
+      v-model="brightness"
+      @change="handleChange"
+    />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@/assets/scss/variables";
+
+.s-wrapper {
+  display: flex;
+  justify-content: center;
+
+  .slider {
+    width: 20rem;
+    margin: 0 auto;
+    height: 3rem;
+    outline: none;
+    appearance: none;
+    border-radius: 3rem;
+    border: 0.25rem solid variables.$mantle;
+
+    background: linear-gradient(
+      90deg,
+      variables.$red v-bind("sliderValue"),
+      variables.$mantle v-bind("sliderValue")
+    );
+
+    &::-webkit-slider-thumb {
+      cursor: pointer;
+      appearance: none;
+      width: 2rem;
+      height: 2rem;
+    }
+  }
+}
+</style>
